39176998baed86d22ec14983ea5b4c63c7f6f90d
Lena <pjanvalena333@gmail.com>
2023-06-04 03:31:32 +0500

    Added adaptive for grayWavyLines

--- adaptive.js
+++ adaptive.js
@@ -12,7 +12,7 @@ function offset(el) {
   };
 }
 
-function imgPositions() {
+const imgFeaturesBlueEllipsePos = () => {
   const imgFeaturesBlueEllipse = document.querySelector(
     "#img-features-blue-ellipse"
   );
@@ -26,7 +26,9 @@ function imgPositions() {
     positionImgFeaturesBlueEllipse.left -
     imgFeaturesBlueEllipse.clientWidth / 4;
   imgFeaturesBlueEllipse.style.cssText = `position: absolute; top: ${imgFeaturesBlueEllipseTop}px; left: ${imgFeaturesBlueEllipseLeft}px;`;
+};
 
+const grayBackgroundPos = () => {
   const grayBackground = document.querySelector("#gray-background");
   const positionGrayBackground = offset(
     document.querySelector(".full-equipment")
@@ -35,23 +37,49 @@ function imgPositions() {
     positionGrayBackground.top -
     (grayBackground.height - positionGrayBackground.height) / 2;
   grayBackground.style.cssText = `position: absolute; top: ${grayBackgroundTop}px; right: 0; width: ${60}%;`;
+};
 
+const whiteDecor1Pos = () => {
   const whiteDecor1 = document.querySelector("#white-decor1");
   const whiteDecor1Position = offset(
     document.querySelector("#header-main-image")
   );
-  let whiteDecor1Top = whiteDecor1Position.top + whiteDecor1Position.height * 1.1 / 2;
+  let whiteDecor1Top =
+    whiteDecor1Position.top + (whiteDecor1Position.height * 1.1) / 2;
   let whiteDecor1Left = whiteDecor1Position.left - whiteDecor1.width * 1.1;
   whiteDecor1.style.cssText = `position: absolute; top: ${whiteDecor1Top}px; left: ${whiteDecor1Left}px;`;
+};
 
-  const whiteDecor2 =  document.querySelector("#white-decor2");
+const whiteDecor2Pos = () => {
+  const whiteDecor2 = document.querySelector("#white-decor2");
   const whiteDecor2Position = offset(
     document.querySelector("#header-main-image")
   );
-  let whiteDecor2Top = whiteDecor2Position.top + whiteDecor2Position.height + whiteDecor2.height;
+  let whiteDecor2Top =
+    whiteDecor2Position.top + whiteDecor2Position.height + whiteDecor2.height;
   let whiteDecor2Left = whiteDecor2Position.left * 1.05;
   whiteDecor2.style.cssText = `position: absolute; top: ${whiteDecor2Top}px; left: ${whiteDecor2Left}px;`;
+};
 
+const greyWavyLinesPos = () => {
+  const greyWavyLines = document.querySelector("#grey-wavy-lines");
+  const greyWavyLinesPosition = offset(
+    document.querySelector(".examples-of-work")
+  );
+  let greyWavyLinesTop =
+    greyWavyLinesPosition.top -
+    (greyWavyLines.height - greyWavyLinesPosition.height) / 2;
+  greyWavyLines.style.cssText = `position: absolute; top: ${greyWavyLinesTop}px; left: 0; height: 90%;`;
+};
+
+
+
+function imgPositions() {
+  imgFeaturesBlueEllipsePos();
+  grayBackgroundPos();
+  whiteDecor1Pos();
+  whiteDecor2Pos();
+  greyWavyLinesPos();
 }
 
 window.addEventListener("load", () => {
