48f43c46936844348ee8e4ad951aa2fbf49d25ea
Lena <pjanvalena333@gmail.com>
2023-06-04 01:17:17 +0500

    Added some adaptive design

---
+++ adaptive.js
@@ -0,0 +1,34 @@
+function offset(el) {
+  var rect = el.getBoundingClientRect();
+  var scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  console.log(rect);
+  return {
+    top: rect.top + scrollTop,
+    left: rect.left + scrollLeft,
+    center: rect.left + (rect.right - rect.left) / 2,
+  };
+}
+
+function imgPositions() {
+  const imgBlueEllipse = document.querySelector("#img-blue-ellipse");
+
+  const imgFeaturesBlueEllipse = document.querySelector(
+    "#img-features-blue-ellipse"
+  );
+  const positionImgFeaturesBlueEllipse = offset(
+    document.querySelector("#position-img-features-blue-ellipse")
+  );
+  let imgFeaturesBlueEllipseTop =
+    positionImgFeaturesBlueEllipse.top -
+    0.2 * imgFeaturesBlueEllipse.clientHeight;
+  let imgFeaturesBlueEllipseLeft =
+    positionImgFeaturesBlueEllipse.left -
+    imgFeaturesBlueEllipse.clientWidth / 4;
+  imgFeaturesBlueEllipse.style.cssText = `position: absolute; top: ${imgFeaturesBlueEllipseTop}px; left: ${imgFeaturesBlueEllipseLeft}px;`;
+
+  const grayBackground = document.querySelector("#gray-background");
+}
+
+window.addEventListener("load", imgPositions);
+window.addEventListener("resize", imgPositions);
