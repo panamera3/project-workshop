3978c2aa4dadc990899b6f25c284825a6ec3d9b8
Lena <pjanvalena333@gmail.com>
2023-06-08 14:20:18 +0500

    It works opening/closing a modal window, prohibiting scrolling when opening a modal window

--- modal.js
+++ modal.js
@@ -1,16 +1,29 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var scrollbar = document.body.clientWidth - window.innerWidth + "px";
-  console.log(scrollbar);
-  document
-    .querySelector('[href="#openModal"]')
-    .addEventListener("click", function () {
-      document.body.style.overflow = "hidden";
-      document.querySelector("#openModal").style.marginLeft = scrollbar;
-    });
-  document
-    .querySelector('[href="#close"]')
-    .addEventListener("click", function () {
-      document.body.style.overflow = "visible";
-      document.querySelector("#openModal").style.marginLeft = "0px";
-    });
-});
+// Получить модальный
+var modal = document.getElementById("myModal");
+
+// Получить кнопку, которая открывает модальный
+var btn = document.getElementById("myBtn");
+// Когда пользователь нажимает на кнопку, откройте модальный
+btn.onclick = () => {
+  modal.style.display = "block";
+  document.body.style.overflow = "hidden";
+  document.documentElement.style.overflow = "hidden";
+};
+
+// Получить элемент <span>, который закрывает модальный
+var span = document.getElementsByClassName("close")[0];
+// Когда пользователь нажимает на <span> (x), закройте модальное окно
+span.onclick = () => {
+  modal.style.display = "none";
+  document.body.style.overflow = "";
+  document.documentElement.style.overflow = "";
+};
+
+// Когда пользователь щелкает в любом месте за пределами модального, закройте его
+window.onclick = (event) => {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    document.body.style.overflow = "";
+    document.documentElement.style.overflow = "";
+  }
+};
