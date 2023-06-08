dfdfb94ac64885e76beb7be172cd3cd90a76191d
Lena <pjanvalena333@gmail.com>
2023-06-08 15:34:08 +0500

    Added adaptive image for technology image for phones and reasons

--- modal.js
+++ modal.js
@@ -2,7 +2,7 @@
 var modal = document.getElementById("myModal");
 
 // Получить кнопку, которая открывает модальный
-var btn = document.getElementById("myBtn");
+var btn = document.getElementById("menuBtn");
 // Когда пользователь нажимает на кнопку, откройте модальный
 btn.onclick = () => {
   modal.style.display = "block";
@@ -14,7 +14,7 @@ btn.onclick = () => {
 var span = document.getElementsByClassName("close")[0];
 // Когда пользователь нажимает на <span> (x), закройте модальное окно
 span.onclick = () => {
-  modal.style.display = "none";
+  modal.style.display = "none"; 
   document.body.style.overflow = "";
   document.documentElement.style.overflow = "";
 };
