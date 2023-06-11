a3e22f24d4fe0c3aa429ce25fc5508bf099e09dc
Lena <pjanvalena333@gmail.com>
2023-06-11 22:30:30 +0500

    Fixed slider height

--- slider.js
+++ slider.js
@@ -1,43 +1,40 @@
-const items = document.querySelectorAll('.reason');
+const items = document.querySelectorAll(".reason");
 const itemCount = items.length;
-const nextItem = document.querySelector('.next');
-const previousItem = document.querySelector('.previous');
+const nextItem = document.querySelector(".next");
+const previousItem = document.querySelector(".previous");
 let count = 0;
 
-function showNextItem() {
-  items[count].classList.remove('active-reason');
-
-  if(count < itemCount - 1) {
+const showNextItem = () => {
+  items[count].classList.remove("active-reason");
+  if (count < itemCount - 1) {
     count++;
   } else {
     count = 0;
   }
+  items[count].classList.add("active-reason");
+  window.dispatchEvent(new Event("resize"));
+};
 
-  items[count].classList.add('active-reason');
-}
-
-function showPreviousItem() {
-  items[count].classList.remove('active-reason');
-
-  if(count > 0) {
+const showPreviousItem = () => {
+  items[count].classList.remove("active-reason");
+  if (count > 0) {
     count--;
   } else {
     count = itemCount - 1;
   }
+  items[count].classList.add("active-reason");
+  window.dispatchEvent(new Event("resize"));
+};
 
-  items[count].classList.add('active-reason');
-}
-
-function keyPress(e) {
+const keyPress = (e) => {
   e = e || window.event;
-  
-  if (e.keyCode == '37') {
+  if (e.keyCode == "37") {
     showPreviousItem();
-  } else if (e.keyCode == '39') {
+  } else if (e.keyCode == "39") {
     showNextItem();
   }
-}
+};
 
-nextItem.addEventListener('click', showNextItem);
-previousItem.addEventListener('click', showPreviousItem);
-document.addEventListener('keydown', keyPress);
+nextItem.addEventListener("click", showNextItem);
+previousItem.addEventListener("click", showPreviousItem);
+document.addEventListener("keydown", keyPress);
--- sliderPhone.js
+++ sliderPhone.js
@@ -4,7 +4,7 @@ const nextItemPhones = document.querySelector(".next-phones");
 const previousItemPhones = document.querySelector(".previous-phones");
 let countPhones = 0;
 
-function shownextItemPhones() {
+const shownextItemPhones = () => {
   itemsPhones[countPhones].classList.remove("active-reason-phone");
   if (countPhones < itemPhonesCount - 1) {
     countPhones++;
@@ -12,9 +12,9 @@ function shownextItemPhones() {
     countPhones = 0;
   }
   itemsPhones[countPhones].classList.add("active-reason-phone");
-}
+};
 
-function showpreviousItemPhones() {
+const showpreviousItemPhones = () => {
   itemsPhones[countPhones].classList.remove("active-reason-phone");
   if (countPhones > 0) {
     countPhones--;
@@ -22,17 +22,17 @@ function showpreviousItemPhones() {
     countPhones = itemPhonesCount - 1;
   }
   itemsPhones[countPhones].classList.add("active-reason-phone");
-}
+};
 
-function keyPress(e) {
+const keyPressPhone = (e) => {
   e = e || window.event;
   if (e.keyCode == "37") {
     showpreviousItemPhones();
   } else if (e.keyCode == "39") {
     shownextItemPhones();
   }
-}
+};
 
 nextItemPhones.addEventListener("click", shownextItemPhones);
 previousItemPhones.addEventListener("click", showpreviousItemPhones);
-document.addEventListener("keydown", keyPress);
+document.addEventListener("keydown", keyPressPhone);
